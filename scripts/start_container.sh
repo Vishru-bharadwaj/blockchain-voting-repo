0609feb0b6feda3fc55129b62788f8832b6ecea9
vishrutha bharadwaj h s <vishruthabharadwajhs@vishruthas-MacBook-Pro-2.local>
2025-07-06 15:58:39 +0530

    Wrong Image Fixed

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -7,9 +7,9 @@ IMAGE_TAG=$(cat /home/ec2-user/blockchain-voting-app/imagedefinitions.json | jq 
 
 # Pull the latest image
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 084828587056.dkr.ecr.us-east-1.amazonaws.com
-docker pull $REPO_URI:$IMAGE_TAG
+docker pull $REPO_URI:latest
 
 # Start container
-docker run -d --rm --name blockchain-voting-container -p 80:5000 $REPO_URI:$IMAGE_TAG
+docker run -d --rm --name blockchain-voting-container -p 80:5000 $REPO_URI:latest
 
 echo "Container started"
