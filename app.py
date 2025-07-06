f08d80aac2b8605e10c59882ee8ce336dbc8cab6
vishrutha bharadwaj h s <vishruthabharadwajhs@vishruthas-MacBook-Pro-2.local>
2025-07-06 15:34:57 +0530

    code change by pramod

--- app.py
+++ app.py
@@ -181,5 +181,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == '__main__':
+    print("-------------app is starting------------")
     app.run(host='0.0.0.0', port=5000, debug=True)
 
